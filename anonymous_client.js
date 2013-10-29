421f36f965c59b36a03b10808c4b8c9ec5ebe408
Codeklopper <basvvliet@gmail.com>
2013-10-29 12:03:03 +0100

    Update anonymous_client.js
    Replaced login function on client

--- anonymous_client.js
+++ anonymous_client.js
@@ -1,8 +1,12 @@
 (function() {
-  Meteor.loginAnonymously = function(fn) {
-    Meteor.call('login', {anonymous: true}, function(err, result) {
-      Meteor.accounts.makeClientLoggedIn(result.id, result.token);
-      fn && fn();
-    });
-  }
+	Meteor.loginAnonymously = function(fn) {
+    	Meteor.call('login', {anonymous: true}, function(err, result) {
+            if(!err) {
+            	if(!Meteor.user()){
+            		Meteor.loginWithToken(result.token);	
+            	}
+                fn && fn(err,result)
+            }
+    	});
+  	}
 })();
